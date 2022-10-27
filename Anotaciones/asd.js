c039e0ed322be4064f9502128164f987753756f1
Flor <flomerfer64@gmail.com>
2022-10-26 23:18:53 -0300

    desafio 3

---
+++ Anotaciones/asd.js
@@ -0,0 +1,32 @@
+//Suma simple
+
+// function sumar(a, b){
+//     console.log(a+b) ;
+// }
+
+// sumar(23,43);
+
+
+// Sumar con array
+
+// function sumar(nums) {
+//     console. log(nums);
+//     console. log(nums[e] + nums[1]);
+// }
+// sumar ([1, 2]);
+
+// Otra forma con funcion anonima
+
+// const sumar = function (num1, num2){
+//     console.log(num1 + num2);
+// }
+// sumar (2, 7);
+
+
+// const sumar = function(num1, num2) {
+//     return num1 + num2; I
+// }
+// let suma = sumar(2, 7);
+// console.log(suma);
+
+    
